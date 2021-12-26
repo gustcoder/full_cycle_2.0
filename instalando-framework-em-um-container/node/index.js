88011ac1a764abc6acf51c9005d3a19634820d0a
gustcoder <gustavofaramos@gmail.com>
2021-12-26 00:02:41 -0300

    Node vs MySQL + dockerize

--- instalando-framework-em-um-container/node/index.js
+++ instalando-framework-em-um-container/node/index.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people (name) VALUES ('Gustavo');`
+connection.query(sql)
+connection.end()
+
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle</h1>')
+    res.send('<h1>Full Cycle dasdasdas</h1>')
 })
 
 app.listen(port, () => {
